20a1ac7559ab0899bc8f06910c95447761c4febb
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-03-02 23:00:28 +0700

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,1 +1,20 @@
+def translate_word() -> None:
 
+    '''
+    Reads a dictionary of N Russian-English word pairs, then translates the phrase.
+    If a word is not in the dictionary, it remains unchanged.
+    Return: None
+    '''
+
+    number = int(input())
+    ru_eng_dict = {}
+    for i in range(number):
+        words = input().split()
+        ru, eng = words
+        ru_eng_dict[ru] = eng
+    stroka = input().split()
+    trans_stroka = [ru_eng_dict.get(slovo, slovo) for slovo in stroka]
+    print(' '.join(trans_stroka))
+
+if __name__ == "__main__":
+    translate_word()

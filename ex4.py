4f96c1ff84d8b5b8df1f42a5255ada4313eb96a3
spiderbedo <134661658+spiderbedo@users.noreply.github.com>
2026-03-12 02:30:53 +0700

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,1 +1,22 @@
+def find_shape() -> None:
+    """
+    Reads the lines, then determines the shape of this item and displays it.
+    If the item is not found, the word itself is displayed.
+    """
+  
+    n = int(input())
+    object_shape = {}
+    for i in range(n):
+        line = input().strip()
+        parts = line.split()
+        shape = parts[0]
+        items = parts[1:]
+        for it in items:
+            object_shape[it] = shape
+    target = input().strip()
+    result = object_shape.get(target, target)
+    print(result)
 
+
+if __name__ == "__main__":
+    find_shape()
